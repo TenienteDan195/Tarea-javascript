34a7a4edb04908610c5c9a682b23436dd0b54a9c
TenieteDan195 <luislopeojeda@gmail.com>
2022-09-03 21:04:04 -0500

    tarea

---
+++ Index.js
@@ -0,0 +1,18 @@
+let nombre = prompt("ingresa el nombre");
+let edad = prompt("ingrsa tu edad");
+
+let mensaje = "Bienvenid@ " + nombre + " tienes " + edad;
+
+let respuesta = prompt("¿estas buscando lentes?")
+
+if (respuesta === "si") {
+    alert("aqui encontraras lo que buscas");
+}else{
+    alert("date una vuelta puede que encuentres algo de tu agrado");
+}
+
+
+
+
+
+
--- Tarea_ con_for/Index.js
+++
@@ -1,9 +0,0 @@
-
-let numero = parseInt(prompt("ingrese numero"));
-
-for(let i = 0; i<= 30; i++){
-    let multiplicacion = numero * i;
-    let mensaje = `${numero} * ${i} = ${multiplicacion}`;
-
-    alert(mensaje);
-}  
